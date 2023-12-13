e915f761dbf6bf9b5408009b46c13dc021ecc6b1
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-12-13 12:09:36 +0300

    Update AppUserRegisterDtos.cs

--- EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDtos.cs
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDtos.cs
@@ -10,7 +10,7 @@ namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; } 
         public string Username { get; set; } 
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
