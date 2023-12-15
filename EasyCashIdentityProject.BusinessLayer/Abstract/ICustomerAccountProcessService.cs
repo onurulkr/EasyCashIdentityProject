88cda625dcbc6893dddbfd4cd0ea892a8fabce12
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-12-15 18:36:45 +0300

    Update ICustomerAccountProcessService.cs

--- EasyCashIdentityProject.BusinessLayer/Abstract/ICustomerAccountProcessService.cs
+++ EasyCashIdentityProject.BusinessLayer/Abstract/ICustomerAccountProcessService.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks; 
 
 namespace EasyCashIdentityProject.BusinessLayer.Abstract
-{
+{ 
     public interface ICustomerAccountProcessService : IGenericService<CustomerAccountProcess> 
     { 
     }
